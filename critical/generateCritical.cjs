f090e0575db10c645b44d65dad7a6425525bbfe9
Lukasz17git <lucas.m2295@gmail.com>
2023-05-18 16:17:58 +0200

    good critical css now

--- critical/generateCritical.cjs
+++ critical/generateCritical.cjs
@@ -1,5 +1,5 @@
 const fs = require('fs')
-
+const basePath = 'oxygen_images_app/'
 /*
 
 "critical": "node critical/generateCritical.cjs"
@@ -12,37 +12,38 @@ const scriptStart = '<script>document.body.innerHTML+= `<div id="critical">'
 const scriptEnd = '</div>`</script>\n'
 
 function generateCriticalAssets(critical) {
-    const imagesPath = 'CriticalImages'
-    const iconsPath = 'CriticalIcons'
-    const imagesCurrentPath = './public/' + imagesPath
-    const iconsCurrentPath = './public/' + iconsPath
-    critical = critical + `<style>#critical>img[src^='/${imagesPath}/'],#critical>img[src^='/${iconsPath}/']{display:none;}</style>`
-    const lastText = scriptEnd
-    const txtOutputPath = './critical/critical.txt'
-    fs.readdir(imagesCurrentPath, (err, files) => {
-        files.forEach(file => critical += `<img src="/${imagesPath}/${file}" alt='${file}'>`)
-        fs.readdir(iconsCurrentPath, (err, files) => {
-            files.forEach(file => critical += `<img src="/${iconsPath}/${file}" alt='${file}'>`)
-            critical += lastText
-            fs.writeFile(txtOutputPath, critical, (error) => error && console.log(error))
-        })
-    })
+   const imagesPath = basePath + 'CriticalImages'
+   const iconsPath = basePath + 'CriticalIcons'
+   const imagesCurrentPath = './public/CriticalImages'
+   const iconsCurrentPath = './public/CriticalIcons'
+   critical = critical + `<style>#critical>img[src^='/${imagesPath}/'],#critical>img[src^='/${iconsPath}/']{display:none;}</style>`
+   const lastText = scriptEnd
+   const txtOutputPath = './critical/critical.txt'
+   fs.readdir(imagesCurrentPath, (err, files) => {
+      files.forEach(file => critical += `<img src="/${imagesPath}/${file}" alt='${file}'>`)
+      fs.readdir(iconsCurrentPath, (err, files) => {
+         files.forEach(file => critical += `<img src="/${iconsPath}/${file}" alt='${file}'>`)
+         critical += lastText
+         fs.writeFile(txtOutputPath, critical, (error) => error && console.log(error))
+      })
+   })
 }
 
 function generateCriticalFonts() {
-    const fontsPath = './public/Fonts'
-    const formatsPriority = ['woff2', 'woff', 'ttf']
-    fs.readdir(fontsPath, (err, files) => {
-        const fonts = {}
-        files.forEach(file => {
-            const fontName = file.slice(0, file.lastIndexOf('.'))
-            const extension = file.slice(file.lastIndexOf('.') + 1)
-            if (!fonts[fontName]) fonts[fontName] = {}
-            fonts[fontName][extension] = file
-        })
-        const bestSuportedFonts = Object.values(fonts).map(font => font[formatsPriority.find(fmt => font[fmt])])
-        let critical = bestSuportedFonts.reduce((c, font) => c + `<link rel="preload" href="/Fonts/${font}" as="font" type="font/${font.slice(font.lastIndexOf('.') + 1)}" crossorigin>`, scriptStart)
-        critical = '\n' + critical
-        generateCriticalAssets(critical)
-    })
+   const fontsPath = basePath + 'Fonts'
+   const fontsCurrentPath = './public/Fonts'
+   const formatsPriority = ['woff2', 'woff', 'ttf']
+   fs.readdir(fontsCurrentPath, (err, files) => {
+      const fonts = {}
+      files.forEach(file => {
+         const fontName = file.slice(0, file.lastIndexOf('.'))
+         const extension = file.slice(file.lastIndexOf('.') + 1)
+         if (!fonts[fontName]) fonts[fontName] = {}
+         fonts[fontName][extension] = file
+      })
+      const bestSuportedFonts = Object.values(fonts).map(font => font[formatsPriority.find(fmt => font[fmt])])
+      let critical = bestSuportedFonts.reduce((c, font) => c + `<link rel="preload" href="/${fontsPath}/${font}" as="font" type="font/${font.slice(font.lastIndexOf('.') + 1)}" crossorigin>`, scriptStart)
+      critical = '\n' + critical
+      generateCriticalAssets(critical)
+   })
 }
