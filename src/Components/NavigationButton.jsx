726eb32cd538d13d77b132440e0aa8ec22689f3e
Lukasz17git <lucas.m2295@gmail.com>
2023-05-18 14:14:38 +0200

    more tiny UI changes

--- src/Components/NavigationButton.jsx
+++ src/Components/NavigationButton.jsx
@@ -5,7 +5,7 @@ const NavigationButton = ({ value, isCurrentPage, onClick }) => {
       <button
          onClick={() => onClick(value)}
          role="navigation"
-         className={`min-w-32 h-32 p-8 br-4 frcc tf-extrabold tc-main-light bg-grey-light ${isCurrentPage ? 'bc-main-extralight oc-main-extralight ow-1' : ''}`}
+         className={`min-w-32 h-32 p-8 br-4 frcc tf-extrabold tc-main-light bg-grey-light ${isCurrentPage ? 'bc-main-extralight bw-1 oc-main-extralight ow-1' : ''}`}
       >
          {value}
       </button>
--- src/Sections/Gallery/Components/CategoryImageChip.jsx
+++ src/Sections/Gallery/Components/CategoryImageChip.jsx
@@ -2,7 +2,7 @@
 
 const CategoryImageChip = ({ category }) => {
    return (
-      <div className="pos-a t-8 l-8 h-32 frcc br-12 px-10 bg-overlay max-w-[70%] tw oh">
+      <div className="pos-a t-8 l-8 h-32 frcc br-10 px-10 bg-overlay max-w-[70%] tw oh">
          <b>{category || 'All Categories'}</b>
       </div>
    )
